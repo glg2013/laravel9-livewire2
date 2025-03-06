2ee21ea9d5aaa93cfc75d0af82fa8be1d585291e
fairy_shrimp <fengniancong@163.com>
2025-03-06 21:30:38 +0800

    引入livewire开发包

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <livewire:styles />
 </head>
 <body>
     <div id="app">
@@ -79,5 +81,7 @@
             @yield('content')
         </main>
     </div>
+
+    <livewire:scripts />
 </body>
 </html>
