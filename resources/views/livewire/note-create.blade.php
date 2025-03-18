b0d1d662431a294731de4a795d0673becc7137d5
gaofeng <57412960@qq.com>
2025-03-18 10:41:52 +0800

    4.添加留言表的创建和测试

--- resources/views/livewire/note-create.blade.php
+++ resources/views/livewire/note-create.blade.php
@@ -1,3 +1,20 @@
 <div>
-    {{-- Be like water. --}}
+    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <div class="container mt-1">
+        @include('shared._errors')
+
+        <form wire:submit.prevent="createNote">
+            <!-- 文本区域 -->
+            <div class="mb-3">
+                <label for="note-content" class="form-label">填写留言：</label>
+                <textarea class="form-control @error("content") is-invalid @enderror " id="note-content" rows="2" placeholder="请输入留言内容..."
+                          wire:model.defer="content"
+                ></textarea>
+            </div>
+
+            <!-- 提交按钮 -->
+            <button type="submit" class="btn btn-primary mb-2">提交</button>
+        </form>
+    </div>
+
 </div>
--- resources/views/livewire/note-index.blade.php
+++ resources/views/livewire/note-index.blade.php
@@ -2,6 +2,9 @@
     {{-- The best athlete wants his opponent at his best. --}}
     @include('shared._message')
 
+    <livewire:note-create />
+    <hr>
+
     @foreach($notes as $note)
         <livewire:note-single :note="$note" :key="$note->id" />
     @endforeach
