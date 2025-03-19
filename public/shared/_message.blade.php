6c493b35b0a414802228a79e8e3d16e8eeb9c1cc
gaofeng <57412960@qq.com>
2025-03-19 10:47:17 +0800

    1.创建基础的小模板、头像资源

---
+++ public/shared/_message.blade.php
@@ -0,0 +1,9 @@
+@foreach(['info', 'success', 'danger', 'warning'] as $type)
+    @if(session()->has($type))
+        <div class="flash-message">
+            <p class="alert alert-{{ $type }}">
+                {{ session()->get($type) }}
+            </p>
+        </div>
+    @endif
+@endforeach
